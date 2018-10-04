7a854aa9c085c7b7dc77ae5b8b52af4508c3b5c5
Jure Bajt <jure.bajt@gmail.com>
2018-10-04 05:55:04 +0200

    Add  candidate details component.

--- src/app/features/coffee-list/coffee-list.module.ts
+++ src/app/features/coffee-list/coffee-list.module.ts
@@ -5,11 +5,13 @@ import {CoffeeListRoutingModule} from './coffee-list-routing.module';
 import {CoffeeListView} from './views/coffee-list/coffee-list.view';
 import {SharedModule} from '../../shared/shared.module';
 import {CoffeeCandidateComponent} from './components/coffee-candidate/coffee-candidate.component';
+import {CandidateDetailsComponent} from './components/candidate-details/candidate-details.component';
 
 @NgModule({
     declarations: [
         CoffeeListView,
         CoffeeCandidateComponent,
+        CandidateDetailsComponent,
     ],
     imports: [CoffeeListRoutingModule, SharedModule, CommonModule],
 })
---
+++ src/app/features/coffee-list/components/candidate-details/candidate-details.component.ts
@@ -0,0 +1,17 @@
+import {
+    Component,
+    Input,
+    ChangeDetectionStrategy,
+} from '@angular/core';
+import {Candidate} from '../../types/candidate';
+
+@Component({
+    selector: 'ce-candidate-details',
+    templateUrl: './candidate-details.component.html',
+    styleUrls: ['./candidate-details.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CandidateDetailsComponent {
+    @Input()
+    candidate: Candidate;
+}
